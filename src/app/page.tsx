a0b286a27c76f28c77c3b0b8ae2956c794dacfbd
Myst-ADL <89290578+aidilsaputrakirsan@users.noreply.github.com>
2025-03-28 06:12:01 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+/*
 import { useState } from "react";
 import BlogSection from "@/components/sections/BlogSection";
 import DestinationGallerySection from "@/components/sections/DestinationGallerySection";
@@ -36,3 +36,4 @@ export default function Home() {
     </main>
   );
 }
+*/
